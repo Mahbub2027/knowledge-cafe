504e5b5763e886cf4eb3e3e2440b2c16956c22d2
Mahbub2027 <alam201127@gmail.com>
2023-09-16 01:31:59 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>    
+      <h1 className='text-3xl bg-orange-400 text-white p-2'>Knowledge Cafe</h1>
+    </>
+  )
+}
+
+export default App
