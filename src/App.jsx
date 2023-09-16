373c35ce3e7e26e1f17fcc7f4831d5584c638432
Mahbub2027 <alam201127@gmail.com>
2023-09-16 10:42:18 +0600

    create fake json data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Header from './components/Header/Header'
 
 function App() {
 
   return (
     <>    
-      <h1 className='text-3xl bg-orange-400 text-white p-2'>Knowledge Cafe</h1>
+      <Header></Header>
+      <Blogs></Blogs>
     </>
   )
 }
